4f6af5503fc255e7c0888a59c45443eed9e91f01
Roberts Slisans <rsxdalv@gmail.com>
2023-08-10 12:03:24 +0300

    fix build #3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,9 @@ proj4.defs(
   "+proj=merc +a=6378137 +b=6378137 +lat_ts=0 +lon_0=0 +x_0=0 +y_0=0 +k=1 +units=m +nadgrids=@null +wktext +no_defs +type=crs"
 );
 
-const data = [];
+const data = {
+  disconnects: [] as any,
+};
 
 const disconnects = data.disconnects.map((x) => {
   x.lat = parseFloat(x.lat as string);
@@ -351,7 +353,6 @@ const tweets = [
     type: "video",
     position: [56.73107794824924, 23.015518380006302],
   },
-  
 ];
 
 export default function Home() {
